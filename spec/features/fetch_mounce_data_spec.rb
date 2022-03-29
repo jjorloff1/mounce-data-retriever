c488d90dfdd024e318be41001a609ce93792d3b2
name <email>
2022-03-28 22:21:10 -0500

    Crawls given pages, extracts data, and outputs to .json and .tsv.

--- spec/features/fetch_mounce_data_spec.rb
+++ spec/features/fetch_mounce_data_spec.rb
@@ -1,5 +1,12 @@
 # spec/features/fetch_mounce_data_spec.rb
 
+Entry = Struct.new(:lexical_form, :grk_translit, :simple_translit, :principal_parts, :strongs, :gk_number, :frequency, :mbg_tag, :gloss, :definition)
+FORMAT = /Dictionary: \n(?<lexical_form>.+?)\nGreek transliteration: \n(?<grk_translit>.+?)\nSimplified transliteration: \n(?<simple_translit>.+?)\n(?:Principal Parts: \n(?<principal_parts>.+?)\n)?Numbers\nStrong's number: \n(?<strongs>\d+)\nGK Number: \n(?<gk_number>\d+)\n.+?\nFrequency in New Testament: \n(?<frequency>\d+)\nMorphology of Biblical Greek Tag: \n(?<mbg_tag>.+?)\nGloss: \n(?<gloss>.+?)\nDefinition: \n(?<definition>.+?)\z/
+
+def parse_line(line)
+  line.match(FORMAT) { |m| Entry.new(*m.captures) }
+end
+
 RSpec.describe "Fetch Mounce Data", type: :feature do
   it "Can load the page" do
     visit 'https://www.billmounce.com/greek-dictionary/hebraios'
@@ -10,24 +17,29 @@ RSpec.describe "Fetch Mounce Data", type: :feature do
   end
 
   it "Loads letter pages" do
-    urls = {
-      "α": "https://www.billmounce.com/greek-dictionary/a",
-      "Ἀαρών": "https://www.billmounce.com/greek-dictionary/aaron",
-      "Ἀβαδδών": "https://www.billmounce.com/greek-dictionary/abaddon",
-      "ἀβαρής": "https://www.billmounce.com/greek-dictionary/abares",
-      "ἀββά": "https://www.billmounce.com/greek-dictionary/abba"
-    }
+    urls = JSON.parse(File.read("spec/url_mappings.json"))
+    # expect(urls.keys.length).to be(3)
 
+    data = {}
+    tabbed_data = []
     urls.each do |key, value|
       visit value
 
       expect(page).to have_content key.to_s
 
-      content = find("div.node-content").text
-      puts content
-
-      expect(page).to have_content key.to_s
-      sleep(0.5)
+      content = find("div.node-content").text.gsub("\t"," ")
+      item = parse_line(content).to_h
+      data[key] = item
+      tabbed_data.push(item.values.join("\t"))
+    rescue
+      puts "Unable to Parse #{key} on #{value} - Content:#{content}\n"
+    ensure
+      sleep(0.5) # don't want to slam the server
     end
+
+    puts data
+
+    File.write('dictionary.json', JSON.dump(data).gsub(" ", " "))
+    File.write('dictionary.tsv', tabbed_data.join("\n").gsub(" ", " "))
   end
 end
